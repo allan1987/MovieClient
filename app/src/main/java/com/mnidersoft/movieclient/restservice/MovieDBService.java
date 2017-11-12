96b0c48015ab8b29eb030f1dcea19276041a7941
Allan Leite Menezes Santos <allan.menezes@netshoes.com>
2017-11-11 23:14:49 -0200

    Adapting MovieDBService to use MovieDB API

--- app/src/main/java/com/mnidersoft/movieclient/restservice/MovieDBService.java
+++ app/src/main/java/com/mnidersoft/movieclient/restservice/MovieDBService.java
@@ -1,11 +1,10 @@
 package com.mnidersoft.movieclient.restservice;
 
-import com.mnidersoft.movieclient.model.Movie;
-
-import java.util.List;
+import com.mnidersoft.movieclient.model.MoviesResponse;
 
 import io.reactivex.Flowable;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 /**
  * Created by Allan.Menezes on 11/10/17.
@@ -15,6 +14,8 @@ public interface MovieDBService {
 
     String BASE_URL = "https://api.themoviedb.org/4/";
 
-    @GET()
-    Flowable<List<Movie>> loadMovies();
+    String API_KEY = "1b967111e1e661560ed39ab7cc61230d";
+
+    @GET("discover/movie?vote_average.gte=5&with_release_type=2|3&api_key=" + API_KEY)
+    Flowable<MoviesResponse> loadNowPlayingMovies(@Query("language") String language, @Query("page") int page);
 }
