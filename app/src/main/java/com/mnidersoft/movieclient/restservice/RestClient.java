7a92f37945a714ce0c7c18b228053843d48b82c7
Allan Leite Menezes Santos <allan.menezes@netshoes.com>
2017-11-11 23:16:20 -0200

    Getting language from default locale; loadMovies with specific language and currentPage

--- app/src/main/java/com/mnidersoft/movieclient/restservice/RestClient.java
+++ app/src/main/java/com/mnidersoft/movieclient/restservice/RestClient.java
@@ -3,9 +3,9 @@ package com.mnidersoft.movieclient.restservice;
 import com.mnidersoft.movieclient.core.errors.DeserializationHandler;
 import com.mnidersoft.movieclient.core.errors.RestErrorsHandler;
 import com.mnidersoft.movieclient.core.states.networkerror.NetworkErrorHandler;
-import com.mnidersoft.movieclient.model.Movie;
+import com.mnidersoft.movieclient.model.MoviesResponse;
 
-import java.util.List;
+import java.util.Locale;
 
 import io.reactivex.Flowable;
 import io.reactivex.Scheduler;
@@ -18,15 +18,19 @@ public class RestClient {
 
     MovieDBService mService;
     Scheduler mScheduler;
+    String mLanguage;
 
     public RestClient(MovieDBService service, Scheduler ioScheduler) {
         mService = service;
         mScheduler = ioScheduler;
+
+        Locale locale = Locale.getDefault();
+        if (locale != null) mLanguage = locale.getLanguage();
     }
 
-    public Flowable<List<Movie>> loadMovies() {
+    public Flowable<MoviesResponse> loadMovies(int page) {
         return mService
-                .loadMovies()
+                .loadNowPlayingMovies(mLanguage, page)
                 .subscribeOn(mScheduler)
                 .compose(new NetworkErrorHandler<>())
                 .compose(new RestErrorsHandler<>())
